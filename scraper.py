2c9598e65218b6cd2947f9b46bff79adc11de623
G710 <16511117+G710@users.noreply.github.com>
2022-03-25 20:08:01 +0100

    clean up

---
+++ scraper.py
@@ -0,0 +1,32 @@
+import os
+import csv
+
+from datetime import datetime
+from requests_html import HTMLSession
+
+sources = []
+
+with open("sources.csv", "r") as file:
+    reader = csv.reader(file, delimiter=",")
+    header = next(reader)
+
+    for row in reader:
+        entry = {}
+        for index, item in enumerate(header):
+            entry[item] = row[index]
+        sources.append(entry)
+
+def fetch_news(source):
+    print("Fetching news from " + source["name"])
+    session = HTMLSession()
+    r = session.get(source["url"])
+
+    target_directory = "../newsprioritiestoday-data/raw/" + source["directory"]
+    if not os.path.exists(target_directory):
+        print("Path for " + source["name"] + "does not exist. Creating path.")
+        os.makedirs(target_directory)
+    with open(target_directory + "/" + source["directory"] + "_" + str(datetime.now()) + ".txt", "w") as file:
+        file.write(r.html.raw_html.decode())
+
+for source in sources:
+    fetch_news(source)
