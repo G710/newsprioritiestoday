a8804c6913d70dacd284174c7c9fa68e112e8d3b
G710 <16511117+G710@users.noreply.github.com>
2022-03-28 20:54:17 +0200

    moved sources to data

--- scraper.py
+++ scraper.py
@@ -4,17 +4,20 @@ import csv
 from datetime import datetime
 from requests_html import HTMLSession
 
-sources = []
 
-with open("sources.csv", "r") as file:
-    reader = csv.reader(file, delimiter=",")
-    header = next(reader)
+def get_sources():
+    sources = []
+    with open("../newsprioritiestoday-data/sources.csv", "r") as file:
+        reader = csv.reader(file, delimiter=",")
+        header = next(reader)
 
-    for row in reader:
-        entry = {}
-        for index, item in enumerate(header):
-            entry[item] = row[index]
-        sources.append(entry)
+        for row in reader:
+            entry = {}
+            for index, item in enumerate(header):
+                entry[item] = row[index]
+            sources.append(entry)
+    
+    return sources
 
 def fetch_news(source):
     print("Fetching news from " + source["name"])
@@ -28,5 +31,6 @@ def fetch_news(source):
     with open(target_directory + "/" + source["directory"] + "_" + str(datetime.now()) + ".html", "w") as file:
         file.write(r.html.raw_html.decode())
 
+sources = get_sources()
 for source in sources:
     fetch_news(source)
